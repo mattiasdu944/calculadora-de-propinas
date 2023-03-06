90f43a587ae4d573aba925730bdf651468bec600
ultron <duartemattais3@gmail.com>
2023-03-06 11:28:25 -0400

    End project

---
+++ app/src/main/java/com/example/calculadoradepropinas/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.calculadoradepropinas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    // static - Define algo que se comparte por todos los objetos de la clase
+    private static final NumberFormat formatMoneda = NumberFormat.getCurrencyInstance();
+    private static final NumberFormat formatPorcentaje = NumberFormat.getPercentInstance();
+
+    private double importe = 0.0;
+    private double porcentaje = 0.0;
+
+    private TextView importeTextView;
+    private TextView porcentaTextView;
+    private TextView propinaTextView;
+    private TextView totalTextView;
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        importeTextView = findViewById(R.id.importeTextView);
+        porcentaTextView = findViewById(R.id.porcentajeTextView);
+        propinaTextView = findViewById(R.id.propinaTextView);
+        totalTextView = findViewById(R.id.totalTextView);
+
+        // Show initial values
+        porcentaTextView.setText( formatPorcentaje.format( porcentaje ) );
+        propinaTextView.setText( formatMoneda.format( 0.0 ) );
+        totalTextView.setText( formatMoneda.format(0.0) );
+
+        // Events
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        EditText importeEditText = findViewById(R.id.importeEditText);
+        importeEditText.addTextChangedListener( listenImporteEditText );
+
+        SeekBar porcentajeSeekBar = findViewById(R.id.porcentajeSeekBar);
+        porcentajeSeekBar.setOnSeekBarChangeListener( listenPorcentajeSeekBar );
+
+        // Focus
+        importeEditText.requestFocus();
+
+    }
+
+    // Listeners
+    private final SeekBar.OnSeekBarChangeListener listenPorcentajeSeekBar = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            porcentaje = progress / 100.0;
+            porcentaTextView.setText(formatPorcentaje.format(porcentaje));
+            calcular();
+
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+
+    private final TextWatcher listenImporteEditText = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            try {
+                importe = Double.parseDouble(s.toString()) / 100.0;
+                importeTextView.setText(formatMoneda.format( importe ));
+
+
+            }catch ( NumberFormatException nfe ){
+                importe = 0.0;
+                importeTextView.setText("");
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
+
+    private void calcular(){
+        double propina = porcentaje * importe;
+        double total = importe + propina;
+
+        propinaTextView.setText(formatMoneda.format(propina));
+        totalTextView.setText(formatMoneda.format(total));
+
+    }
+}
