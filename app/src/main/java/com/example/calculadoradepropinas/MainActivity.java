bc8057e4cb7892241b07da9f2dd1c7124606de53
Ultron <duartemattias3@gmail.com>
2023-03-07 19:45:16 +0100

    end project

--- app/src/main/java/com/example/calculadoradepropinas/MainActivity.java
+++ app/src/main/java/com/example/calculadoradepropinas/MainActivity.java
@@ -20,11 +20,14 @@ public class MainActivity extends AppCompatActivity {
 
     private double importe = 0.0;
     private double porcentaje = 0.0;
+    private int numPersonas = 1;
 
     private TextView importeTextView;
     private TextView porcentaTextView;
     private TextView propinaTextView;
     private TextView totalTextView;
+    private TextView numPersonasTextView;
+    private TextView cuotaTextView;
 
 
     @SuppressLint("MissingInflatedId")
@@ -37,11 +40,14 @@ public class MainActivity extends AppCompatActivity {
         porcentaTextView = findViewById(R.id.porcentajeTextView);
         propinaTextView = findViewById(R.id.propinaTextView);
         totalTextView = findViewById(R.id.totalTextView);
+        numPersonasTextView = findViewById(R.id.numPersonasTextView);
+        cuotaTextView = findViewById(R.id.cuotaTextView);
 
         // Show initial values
         porcentaTextView.setText( formatPorcentaje.format( porcentaje ) );
         propinaTextView.setText( formatMoneda.format( 0.0 ) );
         totalTextView.setText( formatMoneda.format(0.0) );
+        cuotaTextView.setText( formatMoneda.format(0.0) );
 
         // Events
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         EditText importeEditText = findViewById(R.id.importeEditText);
@@ -49,6 +55,10 @@ public class MainActivity extends AppCompatActivity {
 
         SeekBar porcentajeSeekBar = findViewById(R.id.porcentajeSeekBar);
         porcentajeSeekBar.setOnSeekBarChangeListener( listenPorcentajeSeekBar );
+
+        EditText numPersonasEditText = findViewById(R.id.numPersonasEditText);
+        numPersonasEditText.addTextChangedListener( listenNumPersonasEditText );
+
 
         // Focus
         importeEditText.requestFocus();
@@ -102,13 +112,37 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private final TextWatcher listenNumPersonasEditText = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            try {
+                numPersonas = Integer.parseInt(s.toString());
+                numPersonasTextView.setText(s.toString());
+            }catch ( NumberFormatException nfe ){
+                numPersonasTextView.setText("1");
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
 
     private void calcular(){
         double propina = porcentaje * importe;
         double total = importe + propina;
+        double cuota = total / numPersonas;
 
         propinaTextView.setText(formatMoneda.format(propina));
+        cuotaTextView.setText(formatMoneda.format(cuota));
         totalTextView.setText(formatMoneda.format(total));
+
 
     }
 }
